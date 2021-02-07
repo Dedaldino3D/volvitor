862176a23c292aaba38a3de4cbfe1f4db54687b4
Dedaldino3D <dedaldinoantonio7@gmail.com>
2021-02-07 18:55:11 +0100

    Add app logo

---
+++ src/components/layout/Logo.js
@@ -0,0 +1,11 @@
+import logo from "../../images/icaller-logo.svg";
+
+export const Logo = () => (
+  <img
+    src={logo}
+    alt="logo"
+    style={{ width: "30px", height: "30px", lineHeight: "70px" }}
+  />
+);
+
+export default Logo;
